9b7543d726a755f334b18ad237fb17cbb70b4b23
S <ricardoliang@gmail.com>
2013-07-18 21:07:48 -0300

    Fixed array arguments.

--- script.c
+++ script.c
@@ -6,27 +6,25 @@
 FILE *popen(const char *, const char *);
 int pclose(FILE *);
 
-static void script_parse(char *buf,
-		uint32_t *x, uint32_t *y, uint32_t *width, uint32_t *height)
+static void script_parse(char *buf, uint32_t xywh[4])
 {
-	uint32_t *output[4] = { x, y, width, height };
 	for (uint32_t i = 0; i < 4; ++i) {
 		char *space = buf;
-		*output[i] = strtoul(buf, &space, 10);
+		xywh[i] = strtoul(buf, &space, 10);
 		buf = space;
 	}
 }
 
 static void script_read(FILE *f)
 {
-	uint32_t x, y, width, height;
+	uint32_t xywh[4];
 	char buf[BUFSIZ];
 
 	for (uint32_t i = 0; i < windows_len; ++i) {
 		if (fgets(buf, BUFSIZ, f) == NULL)
 			break;
-		script_parse(buf, &x, &y, &width, &height);
-		wrapper_move_resize(windows[i], x, y, width, height);
+		script_parse(buf, xywh);
+		wrapper_move_resize(windows[i], xywh);
 	}
 }
 
